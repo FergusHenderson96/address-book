40acf2d70ddc09384f52add30068de5f85517316
FergusHenderson96 <FergusHenderson96@users.noreply.github.com>
2021-01-15 15:45:28 +0000

    updates added

--- src/App.js
+++ src/App.js
@@ -1,40 +1,27 @@
-import React from "react";
+import React, { Component } from 'react';
 import "./App.css";
 
-class App extends React.Component {
+class App extends Component{
   state = {
-    inputText: "",
-    formDetails: [],
+    addresses: []
   };
-  handleInput = (event) => {
-    // getting the value of the input and assigning to the state
-    this.setState({ inputText: event.target.value });
-  };
-  handleSubmit = (event) => {
-    // stop default form behaviour which is to reload the page.
-    // dont want that with react spa!!!!
-    event.preventDefault();
-    this.setState({
-      formDetails: [...this.state.formDetails, this.state.inputText],
-      inputText: "",
-    });
-  };
-  render() {
-    return (
-      <div className="calculator-wrapper">
-          {/* Encouraged but not required you can do it all with inputs */}
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            // by making the state value the SINGLE SOURCE TRUTH we know exactly what the value will be always
-            value={this.state.inputText}
-            onChange={this.handleInput}
-          />
-        </form>
-        <h1>{this.state.inputText}</h1>
-      </div>
-    );
-  }
+addHandler = () => {
+  let storedAddresses = [...this.state.addresses];
+  storedAddresses.push(storedAddresses);
+  this.setState({storedAddresses});
+};
+
+render() {
+  return (
+    <div>
+      {this.state.addresses.map((addresses, index) => {
+        return <p key={index}>{addresses}</p>;
+      })}
+      <button onClick={this.addHandler}>Add address</button>
+    </div>
+  );
+}
+
 }
 
 export default App;
